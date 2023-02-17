88bd24bc82917e5761d57602cf3c5224fe6bf89f
[ISW] <is@israelswilson.com>
2023-02-16 23:14:41 -0500

    Added prompt buttons and generate button

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,21 @@ const Home = () => {
         </div>
         {/* Add this code here*/}
         <div className="prompt-container">
-          <textarea placeholder="start typing here" className="prompt-box" />
-        </div>
+  <textarea
+    placeholder="start typing here"
+    className="prompt-box"
+    value={userInput}
+    onChange={onUserChangedText}
+  />
+  {/* New code I added here */}
+  <div className="prompt-buttons">
+    <a className="generate-button" onClick={null}>
+      <div className="generate">
+        <p>Generate</p>
       </div>
+    </a>
+  </div>
+</div>
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
