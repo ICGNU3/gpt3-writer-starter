7680d12c175ef914c00411f5f4fef9927d84bde3
[ISW] <is@israelswilson.com>
2023-02-18 15:23:05 -0500

    Anotehr attempted fix

--- pages/index.js
+++ pages/index.js
@@ -5,32 +5,32 @@ import { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
-  const [apiOutput, setApiOutput] = useState('')
-const [isGenerating, setIsGenerating] = useState(false)
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
 
-const callGenerateEndpoint = async () => {
-  setIsGenerating(true);
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
   
-  console.log("Calling OpenAI...")
-  const response = await fetch('/api/generate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ userInput }),
-  });
-
-  const data = await response.json();
-  const { output } = data;
-  console.log("OpenAI replied...", output.text)
-
-  setApiOutput(`${output.text}`);
-  setIsGenerating(false);
-}
   const onUserChangedText = (event) => {
     console.log(event.target.value);
     setUserInput(event.target.value);
-    
   };
 
   return (
@@ -44,49 +44,52 @@ const callGenerateEndpoint = async () => {
             <h2>input your goal below, we'll create plan to reach it in 90 days with breaks</h2>
           </div>
         </div>
+        
         {/* Add this code here*/}
-       <div className="prompt-container">
-  <textarea
-    placeholder="start typing here"
-    className="prompt-box"
-    value={userInput}
-    onChange={onUserChangedText}
-  />
-  <div className="prompt-buttons">
-  <a className="generate-button" onClick={callGenerateEndpoint}>
-    <div className="generate">
-      <p>Generate</p>
-    </div>
-  </a>
-</div>
-{apiOutput && (
-  <div className="output">
-    <div className="output-header-container">
-      <div className="output-header">
-        <h3>Output</h3>
-      </div>
-    </div>
-    <div className="output-content">
-      <p>{apiOutput}</p>
-    </div>
-  </div>
-)}
-</div>  // Add the closing curly brace here
-
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        <div className="prompt-container">
+          <textarea
+            placeholder="start typing here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={callGenerateEndpoint}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
-        </a>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>{/* Add the closing curly brace here */}
+
+        <div className="badge-container grow">
+          <a
+            href="https://buildspace.so/builds/ai-writer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="badge">
+              <Image src={buildspaceLogo} alt="buildspace logo" />
+              <p>build with buildspace</p>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
+
